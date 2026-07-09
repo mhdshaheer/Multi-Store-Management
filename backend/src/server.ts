a53901b277442e3fb58ac96fc4c6a9f8147db7e1
Shaheer cp <shaheercp890@gmail.com>
2026-07-10 02:40:49 +0530

    feat(backend) : server is created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,9 @@
-import express from "express";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
