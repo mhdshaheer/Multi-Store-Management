c6ebf789598e8767d21302bd716fa3ec09e84dd3
Shaheer cp <shaheercp890@gmail.com>
2026-07-14 04:41:05 +0530

    feat(frontend) :registraion api integrated

--- frontend/src/app/features/auth/components/register/register.ts
+++ frontend/src/app/features/auth/components/register/register.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth.service';
+import { IRegister } from '../../../../core/models/auth.model';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  private fb = inject(FormBuilder);
+  private _authService = inject(AuthService);
+  private _router = inject(Router);
+
   showPassword = false;
   showConfirmPassword = false;
+
+  registerForm: FormGroup = this.fb.group(
+    {
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+    },
+    {
+      validators: this.passwordMatchValidator,
+    },
+  );
+
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+  get f() {
+    return this.registerForm.controls;
+  }
 
   togglePassword() {
     this.showPassword = !this.showPassword;
@@ -17,5 +52,26 @@ export class Register {
 
   toggleConfirmPassword() {
     this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  register() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+    const registerData: IRegister = {
+      name: this.registerForm.value.name,
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
+    };
+    this._authService.register(registerData).subscribe({
+      next: (res) => {
+        console.log(res.message);
+        this._router.navigate(['/auth/otp']);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
