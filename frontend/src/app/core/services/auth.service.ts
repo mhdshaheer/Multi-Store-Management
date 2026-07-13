f1441cf28de67834d4e9497feacb3b77f4869e41
Shaheer cp <shaheercp890@gmail.com>
2026-07-14 04:20:45 +0530

    feat(frontend) : auth service with endpoint is created

---
+++ frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ILogin, IRegister, IVerifyOtp } from '../models/auth.model';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../models/api.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private _http = inject(HttpClient);
+  private _api = 'http://localhost:5000/api/v1/auth';
+
+  login(loginData: ILogin): Observable<ApiResponse<ILogin>> {
+    return this._http.post<ApiResponse<ILogin>>(`${this._api}/login`, loginData);
+  }
+  logout() {
+    return this._http.post(`${this._api}/logout`, {});
+  }
+  register(registerData: IRegister) {
+    return this._http.post<ApiResponse<IRegister>>(`${this._api}/register`, registerData);
+  }
+  verifyOtp(verifyData: IVerifyOtp) {
+    return this._http.post<ApiResponse<IRegister>>(`${this._api}/verify`, verifyData);
+  }
+  resendOtp(email: string) {
+    return this._http.post<ApiResponse<null>>(`${this._api}/verify`, email);
+  }
+}
--- frontend/src/app/features/auth/components/login/login.ts
+++ frontend/src/app/features/auth/components/login/login.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private fb = inject(FormBuilder);
   showPassword = false;
 
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+  get f() {
+    return this.loginForm.controls;
+  }
   togglePassword(): void {
     this.showPassword = !this.showPassword;
   }
-  login(): void {
-    console.log('Login clicked');
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.value);
+
+    // API Call
+    // this.authService.login(this.loginForm.value).subscribe(...)
   }
 }
